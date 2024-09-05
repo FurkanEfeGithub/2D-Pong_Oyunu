51a77f4f8402ee6d6427f82fc8db2a1c72daf8bc
FurkanEfeGithub <166522460+FurkanEfeGithub@users.noreply.github.com>
2024-09-05 19:42:33 +0300

    Create GameManager.cs

---
+++ GameManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Transform playerLeftPosition,playerRightPosition,ballPosition;
+    public static GameManager gameManager;
+    void Awake()//Awake METODU,START METODUNDAN ÖNCE ÇAGRILIR.ADINI DEGİŞTİREMEZSİN.
+    {
+        gameManager=this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OyunYenidenBasladigindaPosizyonlar()
+    {
+        playerLeftPosition.position=new Vector3(-8,0,0);
+        playerRightPosition.position=new Vector3(8,0,0);
+        ballPosition.GetComponent<Ball>().BaslangictaTopSagaGitsin();
+        ballPosition.GetComponent<Ball>().OyunYenidenBasladigindaBallPozisyonu();
+    }
+}
