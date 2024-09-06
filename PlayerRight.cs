6c8cca3f3e3daebd5ac8b002da383e855be0bba9
FurkanEfeGithub <166522460+FurkanEfeGithub@users.noreply.github.com>
2024-09-06 18:00:09 +0300

    Update PlayerRight.cs

--- PlayerRight.cs
+++ PlayerRight.cs
@@ -20,4 +20,28 @@ public class PlayerRight : MonoBehaviour
         rb.velocity=new Vector2(0,
         Input.GetAxis("VerticalPlayerRight")* 10f);
     }
+    //--------------------KODU DAHA İYİ ANLAMAK İÇİN ÇEŞİTLİ VARYASYON METODLARIM-------------------
+    void PlayerOtoYukariHareketi()
+    {
+        rb.velocity=new Vector2(0,-5);
+    }
+    void PlayerOtoAsagiHareketi()
+    {
+        rb.velocity=new Vector2(0,5);
+    }
+    void TuslarlaYukariAsagiHareket()
+    {
+        rb.velocity=new Vector2(0,
+        Input.GetAxis("Vertical")*10);
+    }
+    void TuslarlaSolaSagaHareket()
+    {
+        rb.velocity=new Vector2(Input.GetAxis("Horizontal"),
+        0);
+    }
+    void TuslarlaYukariAsagiSagaSolaHareket()
+    {
+        rb.velocity=new Vector2(Input.GetAxis("Horizontal"),
+        Input.GetAxis("Vertical"));
+    }
 }
